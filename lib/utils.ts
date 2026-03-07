fcca07d8c98df4a264625e321a348993f528d080
Ryan Adi <ryanadiprasetyo2004@gmail.com>
2026-03-07 15:25:06 +0700

    feat: initial commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
-/** Reusable class name utility. Add clsx + tailwind-merge when using shadcn. */
-export function cn(...classes: (string | undefined | false)[]): string {
-  return classes.filter(Boolean).join(" ");
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
